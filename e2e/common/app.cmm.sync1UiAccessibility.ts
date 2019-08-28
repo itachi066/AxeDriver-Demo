86f1eaeec57266d50d5ce3320a0d66b37e2f2925
itachi066 <eduardorojascampos@gmail.com>
2019-08-28 04:35:22 -0600

    Add files via upload

---
+++ e2e/common/app.cmm.sync1UiAccessibility.ts
@@ -0,0 +1,27 @@
+﻿import { browser, element, by } from 'protractor';
+const AxeBuilder = require('axe-webdriverjs');
+const util = require('util');
+
+const customRuleJson = require('./custom-rule.json');
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 180000;
+
+export class Sync1UiAccessibility {
+
+  validateAccessibility(testPage) {
+    AxeBuilder(browser)
+    //.withRules('link-name')
+    //.exclude('mat-label')
+    //.configure(customRuleJson)
+    .analyze(results => {
+      console.log('Accessibility Violations: ', results.violations.length);
+      console.log('Accessibility incomplete: ', results.incomplete.length);
+      if (results.violations.length > 0 || results.incomplete.length > 0) {
+        console.log(util.inspect(results.violations, { showHidden: true, depth: 100 }));
+        console.log(util.inspect(results.incomplete, { showHidden: true, depth: 100 }));
+      }
+      expect('Axe violations: ' + (results.violations.length + results.incomplete.length) + ' on ' +
+        testPage + ' page').toEqual('Axe violations: ' + 0 + ' on ' + testPage + ' page');
+    });
+  }
+}
